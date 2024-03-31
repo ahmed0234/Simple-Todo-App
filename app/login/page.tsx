cf6d57c6ccea8368a4eb9a352af1c1e6e69f52e7
Ahmed Hassan <95303771+ahmed0234@users.noreply.github.com>
2024-04-01 02:24:33 +0500

    Add files via upload

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs"
 const page = () => {
   return (
     <div className="flex items-center justify-center pt-12">
-        <SignIn afterSignUpUrl={`/`} afterSignInUrl={`/`} signUpUrl="/signup" />
+        <SignIn  />
     </div>
   )
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs"
 const page = () => {
   return (
     <div className="flex items-center justify-center pt-12">
-      <SignUp afterSignUpUrl={`/`} afterSignInUrl={`/`} signInUrl="/login" />
+      <SignUp />
     </div>
   )
 }
